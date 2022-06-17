06d33e5c934e0c3bddc8cee97b1e5584eddff1a1
Aharon Cuevas <aharon@mymbp.lan>
2022-06-17 00:05:00 -0400

    Solución 0203-E1

---
+++ 02 Control de flujo/solucion/ordena_tres_numeros.py
@@ -0,0 +1,32 @@
+a = input('a: ')
+b = input('b: ')
+c = input('c: ')
+
+if a > b:       
+    if a > c:
+        mayor = a
+        if b > c:
+            medio = b
+            menor = c           # a-b-c
+        else:
+            medio = c
+            menor = b           # a-c-b
+    else:
+        mayor = c
+        medio = a
+        menor = b               # c-a-b
+else:
+    if b > c:
+        mayor = b
+        if a > c:
+            medio = a
+            menor = c           # b-a-c
+        else:
+            medio = c
+            menor = a           # b-c-a
+    else:
+        mayor = c
+        medio = b
+        menor = a               # c-b-a
+
+print('Números ordenados: {}-{}-{}'.format(menor, medio, mayor))
